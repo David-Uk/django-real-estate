63c64a15cec2644b39d92b1876a92cde3026ee8e
David Ukelere <david.ukelere291@gmail.com>
2020-11-11 15:09:02 +0100

    Created listings

---
+++ .history/listings/models_20201110215258.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Listing(models.Model):
+    realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
+    title = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zipcode = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    price = models.IntegerField()
+    
--- .history/real_estate/settings_20201108045538.py
+++ .history/real_estate/settings_20201108045538.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'pserver',
     'pages',
     'listings',
-    'realtors',
 ]
 
 MIDDLEWARE = [
