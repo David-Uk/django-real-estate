886882fcb48da872e2954f7cfede0dc0f5017229
David Ukelere <david.ukelere291@gmail.com>
2020-11-12 20:10:41 +0100

    Media and admin setup

--- .history/listings/admin_20201112195812.py
+++ .history/listings/admin_20201112195812.py
@@ -7,10 +7,7 @@ class ListingAdmin(admin.ModelAdmin):
                     'price', 'list_date', 'realtor')
     list_display_links = ('id', 'title')
     list_filter = ('realtor',)
-    list_editable = ('is_published',)
-    search_fields = ('title', 'description', 'address',
-                     'city', 'state', 'zipcode', 'price')
-    list_per_page = 25
+    list_editable = ('is_published')
 
 
 # Register your models here.
--- .history/listings/admin_20201112195848.py
+++ .history/listings/admin_20201112195848.py
@@ -8,9 +8,6 @@ class ListingAdmin(admin.ModelAdmin):
     list_display_links = ('id', 'title')
     list_filter = ('realtor',)
     list_editable = ('is_published',)
-    search_fields = ('title', 'description', 'address',
-                     'city', 'state', 'zipcode', 'price')
-    list_per_page = 25
 
 
 # Register your models here.
