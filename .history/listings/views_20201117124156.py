97271871446d5ff7923ca27b7c1aa76a3537f368
David Ukelere <david.ukelere291@gmail.com>
2020-11-17 20:56:51 +0100

    Completed form fields

---
+++ .history/listings/views_20201117124156.py
@@ -0,0 +1,42 @@
+from django.http import request
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+
+from .models import Listing
+from .choices import price_choices, bedroom_choices, state_choices
+# Create your views here.
+
+
+def index(request):
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+
+    paginator = Paginator(listings, 3)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+
+    context = {
+        'listings': paged_listings
+    }
+    return render(request, 'listings/listings.html', context)
+
+
+def listing(request, listing_id):
+    listing = get_object_or_404(Listing, pk=listing_id)
+
+    context = {
+        'listing': listing
+    }
+
+    return render(request, 'listings/listing.html', context)
+
+
+def search(request):
+    queryset_list = Listing.objects.order_by('-list_date')
+
+    context = {
+        'price_choices':price_choices,
+        'bedroom_choices':bedroom_choices,
+        'bedroom_choices':state_choices,
+        'listings': queryset_list
+    }
+    return render(request, 'listings/search.html',context)
